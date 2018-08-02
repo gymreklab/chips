9be0f5440f2fdbb007491fec5f4d86c30dc61567
gymreklab <melissa.gymrek@gmail.com>
2018-08-01 21:51:13 -0700

    updating test sim

--- test_sim.sh
+++ test_sim.sh
@@ -11,4 +11,6 @@ PEAKS=testpeaks.tab
     --numreads 1000 \
     --readlen 50 \
     --gamma-frag 200,0.5 \
-    --spot 0.0035 --frac 0.0001
+    --spot 0.0035 --frac 0.0001 \
+    --region chr20:45195673-45395941 \
+    --binsize 10000
