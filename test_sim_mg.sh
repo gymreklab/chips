c615c937facd4fb5233f5b33329873b4283bfbbf
gymreklab <melissa.gymrek@gmail.com>
2018-10-12 14:23:05 -0700

    modifying mg test sim

--- test_sim_mg.sh
+++ test_sim_mg.sh
@@ -10,24 +10,27 @@ COLUMN=5
 # spot: fraction of reads in peaks
 # frac: fraction of genome that is bound
 
-./src/asimon simreads \
-    -p ${PEAKS} \
-    -t ${TYPE} \
-    -f ${REFFA} \
-    -c ${COLUMN} \
-    -o ${OUTPREFIX} \
-    --numcopies 10000 \
-    --numreads 100000 \
-    --readlen 100 \
-    --gamma-frag 5.663,44.08 \
-    --spot 0.110237 --frac 0.00416413 \
-    --region chr19:13628710-15628710 \
-    --binsize 200000 \
-    --paired
-
-bowtie2 -x $(echo $REFFA | cut -d'.' -f 1) -1 ${OUTPREFIX}/reads_1.fastq -2 ${OUTPREFIX}/reads_2.fastq > ${OUTPREFIX}/reads.sam
-samtools view -bS ${OUTPREFIX}/reads.sam > ${OUTPREFIX}/reads.bam
-samtools sort ${OUTPREFIX}/reads.bam -o ${OUTPREFIX}/reads.sorted.bam
-samtools index ${OUTPREFIX}/reads.sorted.bam
-igvtools count -z 5 -w 25 -e 0 ${OUTPREFIX}/reads.sorted.bam  ${OUTPREFIX}/reads.sorted.tdf $REFFA
-
+for nc in 10 100 1000 1000 10000
+do
+    ./src/asimon simreads \
+	-p ${PEAKS} \
+	-t ${TYPE} \
+	-f ${REFFA} \
+	-c ${COLUMN} \
+	-o ${OUTPREFIX} \
+	--numcopies ${nc} \
+	--numreads 100000 \
+	--readlen 100 \
+	--gamma-frag 5.663,44.08 \
+	--spot 0.110237 --frac 0.00416413 \
+	--region chr19:13628710-15628710 \
+	--binsize 200000 \
+	--paired
+    
+    bowtie2 -x $(echo $REFFA | cut -d'.' -f 1) -1 ${OUTPREFIX}/reads_1.fastq -2 ${OUTPREFIX}/reads_2.fastq > ${OUTPREFIX}/reads.${nc}.sam
+    samtools view -bS ${OUTPREFIX}/reads.${nc}.sam > ${OUTPREFIX}/reads.${nc}.bam
+    samtools sort ${OUTPREFIX}/reads.${nc}.bam -o ${OUTPREFIX}/reads.${nc}.sorted.bam
+    samtools index ${OUTPREFIX}/reads.${nc}.sorted.bam
+    igvtools count -z 5 -w 25 -e 0 ${OUTPREFIX}/reads.${nc}.sorted.bam  ${OUTPREFIX}/asimon.${nc}.sorted.tdf $REFFA
+    rm ${OUTPREFIX}/reads*.fastq
+done
