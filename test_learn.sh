863207c9999eb953f0fc44f22b0a3524d660a3bd
Ryan Zheng <ryan0225@foxmail.com>
2018-12-31 15:23:06 -0800

    last update 2018

--- test_learn.sh
+++ test_learn.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-BAM=/storage/agoren/nextseq-runs/2017-09-19/aligned/hg19/final-bams/F1796-HNFa3_S21.sorted.flagged.bam
-PEAKS=/storage/agoren/nextseq-runs/2017-09-19/aligned/hg19/tagdir/F1796-HNFa3_S21/peaks.txt #testpeaks.tab
-TYPE=homer
+#BAM=/storage/agoren/nextseq-runs/2017-09-19/aligned/hg19/final-bams/F1796-HNFa3_S21.sorted.flagged.bam
+#PEAKS=/storage/agoren/nextseq-runs/2017-09-19/aligned/hg19/tagdir/F1796-HNFa3_S21/peaks.txt #testpeaks.tab
+BAM=/storage/pandaman/project/asimon-data/ENCFF431YXJ.marked.bam
+PEAKS=/storage/pandaman/project/asimon-data/ENCFF878DLA.bed
+TYPE=bed
+OUTDIR=/home/pandaman/projects/asimon/asimon-chip-sim/test/
 
 ./src/asimon learn \
     -p ${PEAKS} \
     -b ${BAM} \
     -t ${TYPE} \
-    -o test
+    -o ${OUTDIR}
