00926ea82a43e65ae63f455776eb5894546c3d81
gymreklab <melissa.gymrek@gmail.com>
2019-03-15 11:57:46 -0700

    adding encode paired eamples

--- paper-analyses/encode/readme.sh
+++ paper-analyses/encode/readme.sh
@@ -6,5 +6,12 @@ OUTDIR=/storage/mgymrek/chipmunk/encode
 # TODO for loop to run
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo $line
-    #./process_encode.sh ${bamurl} ${bedurl} ${OUTDIR} ${factor} Both
+    bamurl=$(echo $line | cut -f 4 -d',')
+    bedurl=$(echo $line | cut -f 5 -d',')
+    ct=$(echo $line | cut -f 1 -d',')
+    f=$(echo $line | cut -f 2 -d',')
+    bamacc=$(echo $bamurl | cut -d'/' -f 5)
+    bedacc=$(echo $bedurl | cut -d'/' -f 5)
+    factor=${ct}_${f}_${bamacc}_${bedacc}
+    echo ./process_encode.sh ${bamurl} ${bedurl} ${OUTDIR} ${factor} Both
 done < encode_paired_example_datasets.csv
