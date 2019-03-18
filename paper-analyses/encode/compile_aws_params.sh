6307803b2f0a3be6f02ccfab4a591354d3eae957
gymreklab <melissa.gymrek@gmail.com>
2019-03-18 16:40:59 -0700

    adding eval scripts

--- paper-analyses/encode/compile_aws_params.sh
+++ paper-analyses/encode/compile_aws_params.sh
@@ -4,11 +4,11 @@ MDIR=/storage/mgymrek/chipmunk/encode/models
 
 # Sync json results
 cd $MDIR
-aws s3 sync s3://chipmunk-encode-models .
+aws s3 sync s3://chipmunk-encode-models . > /dev/null 2>&1 
 cd -
 
 echo "CellType,Factor,ENCODE_BAM,ENCODE_BED,Model-k,Model-theta,Model-frac,Model-spot,Model-pcr,ModelFile"
 for f in $(ls $MDIR/*.json)
 do
     ./print_model.py $f
-done
+done | sort -k1,1 -k2,2
--- paper-analyses/encode/run_aws_jobs.sh
+++ paper-analyses/encode/run_aws_jobs.sh
@@ -14,10 +14,12 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     # First check if output file exists
     if [ "${rtype}" = "Paired" ]; then
 	outfile=${factor}.paired.json
+	echo "Skipping paired end for now"
+	continue # process paired later... most too big and fail on AWS
     elif [ "${rtype}" = "Single" ]; then
 	outfile=${factor}.json
     fi
-    aws s3 ls s3://chipmunk-encode-models/${outfile}
+    aws s3 ls s3://chipmunk-encode-models/${outfile} > /dev/null
     if [[ $? -eq 0 ]]; then
 	echo "Found file... skipping ${factor}"
 	continue
@@ -29,6 +31,5 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	--job-definition chipmunk-encode:2 \
         --timeout 'attemptDurationSeconds=3600' \
 	--container-overrides 'command=[\"${SCRIPTNAME}\",\"${bamurl}\",\"${bedurl}\",\"${factor}\",\"${rtype}\"],environment=[{name=\"BATCH_FILE_TYPE\",value=\"script\"},{name=\"BATCH_FILE_S3_URL\",value=\"s3://gymreklab-awsbatch/${SCRIPTNAME}\"}]'"
-#    sh -c "${cmd}"
-    echo "${cmd}"
+    sh -c "${cmd}"
 done < encode_datasets_K562_GM12878_clean.csv
