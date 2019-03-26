140a0f9aaa9bd8bb035f32d7e5e1b21e62eda712
gymreklab <melissa.gymrek@gmail.com>
2019-03-26 08:54:23 -0700

    updating power analysis

---
+++ paper-analyses/fig2_power/summ_power_broad.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source params.sh
+factor=$1
+
+GT=/storage/mgymrek/chipmunk/encode/${factor}/${factor}.bed
+numtotal=$(cat $GT | wc -l)
+
+for numreads in $(echo $READNUMS | sed 's/,/ /g')
+do
+#    f=${OUTDIR}/${factor}.${numreads}.bed
+    f=${OUTDIR}/${factor}.${numreads}.${numreads}.broad_peaks.broadPeak
+    numpeaks=$(intersectBed -a $GT -b $f -u | wc -l)
+    echo $numreads $numpeaks $numtotal
+done | awk -v"factor=$factor" '{print factor " " $0}' > ${OUTDIR}/${factor}.broad.power.tab
