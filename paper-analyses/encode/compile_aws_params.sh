13d69dca07d7787c1c99dfef61a2020258845385
gymreklab <melissa.gymrek@gmail.com>
2019-03-29 11:59:57 -0700

    rerunnign encode analysis on chipmunk 1.9

--- paper-analyses/encode/compile_aws_params.sh
+++ paper-analyses/encode/compile_aws_params.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-MDIR=/storage/mgymrek/chipmunk/encode/models
+MDIR=/storage/mgymrek/chipmunk/encode/models-1.9
 
 # Sync json results
-cd $MDIR
-aws s3 sync s3://chipmunk-encode-models . > /dev/null 2>&1 
-cd -
+aws s3 ls s3://chipmunk-encode-models/ | grep json | awk '{print $4}' | \
+    grep "1\.9" | \
+    xargs -n1 -P1 -I% sh -c "aws s3 cp s3://chipmunk-encode-models/% ${MDIR}/"
+
+# Get local snorlax results 
+find /storage/mgymrek/chipmunk/encode/ | grep json | grep "1\.9" | \
+    xargs -n1 -P1 -I% sh -c "cp % ${MDIR}/"
 
 echo "CellType,Factor,ENCODE_BAM,ENCODE_BED,Model-k,Model-theta,Model-frac,Model-spot,Model-pcr,ModelFile"
 for f in $(ls $MDIR/*.json)
