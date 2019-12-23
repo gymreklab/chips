b528db8a6920c98b622c96c5bb1a1a40c70dc6eb
gymreklab <melissa.gymrek@gmail.com>
2019-12-23 15:54:27 -0800

    updating read number workflow

---
+++ paper-analyses/workflows/gather_stats.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Input the three metrics files. Output key values in tab format
+NARROW=$1
+BROAD=$2
+SIM=$3
+
+echo "narrow-recall,narrow-fpr,narrow-medpeaksize,narrow-spot,broad-recall,broad-fpr,broad-medpeaksize,broad-spot,time_s,mem_MB" | \
+    sed 's/,/\t/g'
+
+output=""
+output=$output,$(cat $NARROW | grep recall | cut -d',' -f 2)
+output=$output,$(cat $NARROW | grep fpr | cut -d',' -f 2)
+output=$output,$(cat $NARROW | grep peaksize | cut -d',' -f 2)
+output=$output,$(cat $NARROW | grep splot | cut -d',' -f 2)
+output=$output,$(cat $BROAD | grep recall | cut -d',' -f 2)
+output=$output,$(cat $BROAD | grep fpr | cut -d',' -f 2)
+output=$output,$(cat $BROAD | grep peaksize | cut -d',' -f 2)
+output=$output,$(cat $BROAD | grep splot | cut -d',' -f 2)
+output=$output,$(cat $SIM | grep -v rss | awk -F"\t" '{print $1}')
+output=$output,$(cat $SIM | grep -v rss | awk -F"\t" '{print $4}')
+echo $output | sed 's/,/\t/g'
