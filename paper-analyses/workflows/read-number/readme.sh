375e43b5fe850608d8d9c65d46567fd414d30192
gymreklab <melissa.gymrek@gmail.com>
2020-02-27 10:34:15 -0800

    adding figure 1 notebook

--- paper-analyses/workflows/read-number/readme.sh
+++
@@ -1,9 +0,0 @@
-# TODO notes on CTCF:
-# - peaks in sim data look much broader than in ENCODE data. why?
-# - maybe for CTCF we shouldn't do scale-outliers? some peaks tiny in real data are huge in sim data
-
-# To run workflow, edit params in Snakefile if necessary then run:
-snakemake
-
-# To make an ENCODE tdf for comparison
-igvtools count -z 5 -w 25 -e 0 /storage/mgymrek/chipmunk_round2/encode/GM12878_CTCF_ENCFF124LLI_ENCFF256QBB/GM12878_CTCF_ENCFF124LLI_ENCFF256QBB.flagged.bam /storage/mgymrek/chipmunk_round2/encode/GM12878_CTCF_ENCFF124LLI_ENCFF256QBB/GM12878_CTCF_ENCFF124LLI_ENCFF256QBB.flagged.tdf /storage/resources/dbase/human/hg19/hg19.fa
