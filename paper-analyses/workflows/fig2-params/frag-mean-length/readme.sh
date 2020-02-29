6157be8c3b5a56d73274e38606f829c5a5b3ccbd
gymreklab <melissa.gymrek@gmail.com>
2020-02-28 16:29:07 -0800

    updating snakefile for fraglen

---
+++ paper-analyses/workflows/fig2-params/frag-mean-length/readme.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# Run tests for varying read numbers for TF/HM
+
+TFPEAKS=/storage/pandaman/project/ChIPs-experiments/data/TF-SP1-sim.bed
+HMPEAKS=/storage/pandaman/project/ChIPs-experiments/data/HM-H3K27ac-sim.bed
+TFMODEL=simplified_GM12878_CTCF.json
+HMMODEL=simplified_GM12878_H3K27ac.json
+
+for layout in single paired
+do
+    for style in "TF" "HM"
+    do
+	if [ x"$style" == x"TF" ]; then
+	    MODELFILE=$TFMODEL
+	    PEAKFILE=$TFPEAKS
+	fi
+	if [ x"$style" == x"HM" ]; then
+	    MODELFILE=$HMMODEL
+	    PEAKFILE=$HMPEAKS
+	fi
+	snakemake \
+	    --config MODELFILE=$MODELFILE \
+	    PEAKFILE=$PEAKFILE \
+	    OUTPREFIX=${style}-${layout} \
+	    LAYOUT=${layout} #--delete-all-output
+    done
+done
