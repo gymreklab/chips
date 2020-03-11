314f49d0a9cb815f18e1aa24109a18f634a62f8f
Gymrek Lab <melissa.gymrek@gmail.com>
2020-03-11 15:59:21 -0700

    Update make_src_dist.sh

--- make_src_dist.sh
+++ make_src_dist.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+############## Follow these instructions to make a new release ########
+# Pull changes
+# > git pull origin master
+#
+# Update the version tag (obviously increment this. use "git tag" to see existing versions)
+# > git tag -a v2.0 -m"v2.0"
+#
+# Push tag update
+# > git push origin --tags
+#
+# Run this to make sure tag gets properly reset
+# > ./reconf
+#
+# Package
+# > ./make_src_dist.sh
+#
+# This will create a tarball file: chips-X.X.XXXX.tar.gz
+# Upload this file to Github releases page: https://github.com/gymreklab/chips/releases
+
 die()
 {
     BASE=$(basename "$0")
