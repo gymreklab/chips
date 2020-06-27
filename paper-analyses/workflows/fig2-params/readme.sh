cd7b58cfa2873129cc33879156bd5d05963f7b33
mlamkin7 <michael@alamkin.com>
2020-06-26 23:06:05 -0700

    fig2 params pipeline correct iteration

--- paper-analyses/workflows/fig2-params/readme.sh
+++ paper-analyses/workflows/fig2-params/readme.sh
@@ -4,8 +4,7 @@ EXTRAARGS=$1 # e.g. --delete-all-output to clean
 
 # Run all param workflows
 
-#for usedir in "read-number" "read-length" "pcr" "frag-mean-length"
-for usedir in "pcr"
+for usedir in "read-number" "read-length" "pcr" "frag-mean-length"
 do
     echo "##############" $usedir "#############"
     ./run.sh $usedir $EXTRAARGS
