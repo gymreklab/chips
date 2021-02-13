8814c3502fe58ef4915bfe7f35ae53dfeee5da72
gymreklab <melissa.gymrek@gmail.com>
2021-02-12 20:39:42 -0800

    updating workflow for ischip

--- paper-analyses/workflows/fig1-eval/get_ischip_model_params.py
+++ paper-analyses/workflows/fig1-eval/get_ischip_model_params.py
@@ -13,15 +13,16 @@ mean = k*theta
 var = k*theta**2
 sd = np.sqrt(var)
 
-pcr = 0 # no pcr if the rate is 1
+#pcr = 0 # no pcr if the rate is 1
 #if model["pcr_rate"] < 1:
 #    pcr = 10 # ?
+pcr = 0
 
 spot = int(model["pulldown"]["s"]*100)
 frac = int(model["pulldown"]["f"]*100+1)
 
 # complains if fragment length var is > 1
 # no reads output for pcr 20. setting to 10
-sys.stdout.write(" -L %s:%s --pcr %s --ground %s:%s"%(np.log(mean), 1, pcr, spot, frac))
+sys.stdout.write(" -L %s:%s --pcr %s --ground %s:%s"%(np.log(mean), 0.3, pcr, spot, frac))
 
 
